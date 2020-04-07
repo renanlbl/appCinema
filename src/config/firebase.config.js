be426cb6814cdf58b7a3db66fd2b7e44a00fe2ab
Renan Lima <cursosprogramacoes@gmail.com>
2020-04-07 03:04:13 -0300

    Adiciona estrutura básica do projeto

---
+++ src/config/firebase.config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlqmeDOB4OczB1__nSsTKNOQ7Ovq3740k",
+  authDomain: "appcinema-44f55.firebaseapp.com",
+  databaseURL: "https://appcinema-44f55.firebaseio.com",
+  projectId: "appcinema-44f55",
+  storageBucket: "appcinema-44f55.appspot.com",
+  messagingSenderId: "564119898522",
+  appId: "1:564119898522:web:31220a1b9c12125c3b5223"
+};
+
+export default firebase.initializeApp(firebaseConfig);
